d89c3c927a3eeffcf63555924d854f69ea7b34bf
pashaAbri <39867050+pashaAbri@users.noreply.github.com>
2023-12-12 21:26:51 -0500

    update the analyze.py

--- linux-server/scenarios/demo1/analyze.py
+++ linux-server/scenarios/demo1/analyze.py
@@ -1,37 +1,48 @@
 import pyshark
+import pandas as pd
+import os
 
 
 def analyze_pcap(file_path):
-    # Initialize pyshark FileCapture with debug mode enabled
     cap = pyshark.FileCapture(file_path)
     cap.set_debug(True)
 
+    data = []
+
     try:
-        # Iterate over packets and extract information
         for packet in cap:
             try:
-                # Basic packet data
-                protocol = packet.transport_layer  # Protocol type (TCP/UDP)
-                src_addr = packet.ip.src  # Source IP address
-                src_port = packet[packet.transport_layer].srcport  # Source port
-                dst_addr = packet.ip.dst  # Destination IP address
-                dst_port = packet[packet.transport_layer].dstport  # Destination port
-
-                # Output packet details
-                print(f"Packet: {protocol} {src_addr}:{src_port} -> {dst_addr}:{dst_port}")
-
-            except AttributeError as e:
-                # Handle packets that aren't TCP/UDP or are missing IP layer
-                print("Non TCP/UDP packet detected or missing IP layer.")
-
+                data.append({
+                    'protocol': packet.transport_layer,
+                    'src_addr': packet.ip.src,
+                    'src_port': packet[packet.transport_layer].srcport,
+                    'dst_addr': packet.ip.dst,
+                    'dst_port': packet[packet.transport_layer].dstport
+                })
+            except AttributeError:
+                # Skipping non TCP/UDP packets or packets missing IP layer
+                continue
     except Exception as e:
         print(f"Error processing pcap file: {e}")
-
     finally:
-        # Ensure the capture is closed properly
         cap.close()
+
+    return pd.DataFrame(data)
+
+
+def save_dataframe_to_csv(df, filename):
+    df.to_csv(filename, index=False)
+    print(f"Data saved to {filename}")
 
 
 if __name__ == "__main__":
-    pcap_file = "./output_files/traffic_veth1.pcap"  # Replace with your pcap file path
-    analyze_pcap(pcap_file)
+    pcap_files = ["./output_files/traffic_veth1.pcap",
+                  "./output_files/traffic_veth3.pcap",
+                  "./output_files/traffic_veth5.pcap"]
+
+    for file in pcap_files:
+        df = analyze_pcap(file)
+
+        # Construct a CSV filename based on the pcap filename
+        csv_filename = os.path.splitext(file)[0] + '.csv'
+        save_dataframe_to_csv(df, csv_filename)
