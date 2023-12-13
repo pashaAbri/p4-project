6992cc534966175fa99a5e7b3764ba447b345633
pashaAbri <39867050+pashaAbri@users.noreply.github.com>
2023-12-13 16:37:58 -0500

    updating analyze.py to take into account buffer overflows

--- linux-server/scenarios/demo1/analyze.py
+++ linux-server/scenarios/demo1/analyze.py
@@ -2,9 +2,12 @@ import pyshark
 import pandas as pd
 import os
 
+# Define a threshold payload size that is considered suspicious
+SUSPICIOUS_PAYLOAD_SIZE = 1000  # Adjust this value as necessary
+
 
 def analyze_pcap(file_path):
-    cap = pyshark.FileCapture(file_path)
+    cap = pyshark.FileCapture(file_path, only_summaries=False)
     cap.set_debug(True)
 
     data = []
@@ -12,13 +15,16 @@ def analyze_pcap(file_path):
     try:
         for packet in cap:
             try:
-                data.append({
-                    'protocol': packet.transport_layer,
-                    'src_addr': packet.ip.src,
-                    'src_port': packet[packet.transport_layer].srcport,
-                    'dst_addr': packet.ip.dst,
-                    'dst_port': packet[packet.transport_layer].dstport
-                })
+                # Check if the payload length is greater than the suspicious size
+                if 'TCP' in packet and int(packet.tcp.len) > SUSPICIOUS_PAYLOAD_SIZE:
+                    data.append({
+                        'protocol': packet.transport_layer,
+                        'src_addr': packet.ip.src,
+                        'src_port': packet[packet.transport_layer].srcport,
+                        'dst_addr': packet.ip.dst,
+                        'dst_port': packet[packet.transport_layer].dstport,
+                        'payload_len': packet.tcp.len
+                    })
             except AttributeError:
                 # Skipping non TCP/UDP packets or packets missing IP layer
                 continue
@@ -31,8 +37,11 @@ def analyze_pcap(file_path):
 
 
 def save_dataframe_to_csv(df, filename):
-    df.to_csv(filename, index=False)
-    print(f"Data saved to {filename}")
+    if not df.empty:
+        df.to_csv(filename, index=False)
+        print(f"Data saved to {filename}")
+    else:
+        print(f"No data to save for {filename}")
 
 
 if __name__ == "__main__":
@@ -41,8 +50,12 @@ if __name__ == "__main__":
                   "./output_files/traffic_veth5.pcap"]
 
     for file in pcap_files:
+        print(f"Analyzing {file}...")
         df = analyze_pcap(file)
 
+        # Filter out normal traffic if you only want to save suspicious packets
+        df_suspicious = df[df['payload_len'] > SUSPICIOUS_PAYLOAD_SIZE]
+
         # Construct a CSV filename based on the pcap filename
-        csv_filename = os.path.splitext(file)[0] + '.csv'
-        save_dataframe_to_csv(df, csv_filename)
+        csv_filename = os.path.splitext(file)[0] + '_suspicious.csv'
+        save_dataframe_to_csv(df_suspicious, csv_filename)
