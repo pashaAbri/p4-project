ee0acf54b9921f8a961cdf8e408159bbfd68d315
Ubuntu <ubuntu@ip-172-31-2-120.ec2.internal>
2023-12-13 22:30:02 +0000

    updating the setup file

--- linux-server/scenarios/base/setup.sh
+++ linux-server/scenarios/base/setup.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
+# Remove the compiled P4 program directory
+echo "Cleaning up compiled P4 program directory..."
+rm -rf base.bmv2
+
+# Terminate any running simple_switch processes
+echo "Terminating any running simple_switch processes..."
+pkill simple_switch
+
+# Function to delete a veth pair if it exists
+delete_veth_pair() {
+    if ip link show "$1" > /dev/null 2>&1; then
+        echo "Deleting veth pair: $1 and its peer"
+        sudo ip link delete "$1"
+    else
+        echo "Veth pair $1 does not exist, skipping deletion."
+    fi
+}
+
+# Delete the virtual Ethernet pairs
+delete_veth_pair veth0
+delete_veth_pair veth2
+delete_veth_pair veth4
+
+echo "Cleanup complete."
+
 # Compile the P4 program.
 # In the commands below, the -b option selects bmv2 (Behavioral Model Version 2) as the target,
 # which is the software switch that we will use to run the P4 program.
